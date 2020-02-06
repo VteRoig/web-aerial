e2d2b175d6c6a0ac62d1e935c556fbe5af95b3cc
Vicente Roig <vtroig@gmail.com>
2020-02-06 20:05:48 +0100

    Add files via upload

---
+++ assets/sass/cookies.js
@@ -0,0 +1,10 @@
+<!-- SCRIPT CONTROL DE COOKIES -->
+            <script type="text/javascript">
+            function controlcookies() {
+                    // si variable no existe se crea (al clicar en Aceptar)
+                localStorage.controlcookie = (localStorage.controlcookie || 0);
+            
+                localStorage.controlcookie++; // incrementamos cuenta de la cookie
+                cookie1.style.display='none'; // Esconde la política de cookies
+            }
+            </script>
